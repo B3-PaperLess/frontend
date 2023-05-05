cdbaa791e9d92cf7de87882e7bb23a521c4069ed
Quentin Couderc <quentin.couderc@socotec.com>
2023-05-05 17:25:33 +0200

    feat(install vuetify, axios, disable eslint and create axiosConfig example)

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    requireConfigFile: false,
+    parser: "@babel/eslint-parser",
+  },
+  rules: {
+    // Ajoutez vos règles spécifiques ici
+  },
+};
---
+++ src/axiosConfig.js
@@ -0,0 +1,7 @@
+// export default axios.create({
+//   baseURL: 'http://localhost:8000/api/',
+//   auth: {
+//     username: 'admin',
+//     password: 'password123'
+//   }
+// });
