fdfbca0e53d00fc4b520c88b3de30869213685db
Matthieu Flaceliere <flacelierematthieu@gmail.com>
2023-07-12 22:05:03 +0200

    feat: accueil hors ligne

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,30 +5,48 @@
   </div>
 
   <div class="mt-12 flex flex-row justify-center gap-x-12">
-    <card-home class="rounded-3xl">
+    <card-home class="rounded-3xl text-center mx-48">
+      <template v-slot:content>
+        Découvrez Paperless, la solution idéale pour dématérialiser facilement vos factures et vous conformer aux nouvelles exigences gouvernementales.En tant que services de dématérialisation officiel,nous vous offrons la possibilité de transmettre vos factures électroniquement aux autorités gouvernementales ou à d'autres tier, tout en assurant une vérification minutieuse et une réponse rapide.
+      </template>
+    </card-home>
+  </div>
+  <div class="flex flex-row justify-arround mt-10">
+    <card-home class="rounded-3xl text-center w-1/3 h-40 mx-2">
       <template v-slot:title>
-        Avantage
+        Etape 1
       </template>
 
       <template v-slot:content>
-        Je suis le content
+        Créer votre compte rien de plus simple !
       </template>
 
       <template v-slot:button>
-        <button-default class="border border-gray-300 bg-base-lg">je suis le bouton</button-default>
+        <button-default class="w-1/2 mt-5">
+          <router-link to="/register">
+            Créer un compte
+          </router-link>
+        </button-default>
       </template>
     </card-home>
-
-    <card-home>
+    <card-home class="rounded-3xl text-center w-1/3 h-40 mx-2">
       <template v-slot:title>
-        Je suis le title
+        Etape 2
       </template>
 
       <template v-slot:content>
-        Je suis le content
+        Votre dois être vérifier par un administrateur, vous recevrez un mail lors de la validation de votre compte.
       </template>
     </card-home>
+    <card-home class="rounded-3xl text-center w-1/3 h-40 mx-2">
+      <template v-slot:title>
+        Etape 3
+      </template>
 
+      <template v-slot:content>
+        Envoyez vos factures en toute simplicité !
+      </template>
+    </card-home>
   </div>
 </div>
 </template>
