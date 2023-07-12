669090b9f6c44334aab579aa2ff66faaf6826d3b
Quentin Couderc <quentin.couderc@socotec.com>
2023-07-12 23:15:01 +0200

    feat(create Datatable)

---
+++ src/components/datatable.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th v-for="item in headers" :key="item.value">{{ item }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in items" :key="item.id">
+          <td>{{ item.name }}</td>
+          <td>{{ item.location }}</td>
+          <td>{{ item.state }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'datatable',
+  props: {
+      headers: {
+      type: Array,
+    },
+    items:{
+      type: Array,
+    }
+  }
+};
+</script>
+
+<style>
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th,
+td {
+  border: 1px solid #ddd;
+  padding: 8px;
+  background-color: white;
+}
+
+th {
+  background-color: #f2f2f2;
+}
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,15 +1,41 @@
 <template>
-<div>
-  espace utilisateur
-</div>
-</template>
-
-<script>
-export default {
-  name: "indexHome"
-}
-</script>
-
-<style scoped>
-
-</style>
+  <div class="grid grid-cols-3 px-24">
+    <div class="mt-4 w-fit px-12">
+      <img src="@/assets/image/logo.png" alt="logo" class="w-fit"/>
+    </div>
+    <div class="mt-4 col-span-2">
+      <datatable
+      :headers="headers"
+      :items="items"
+      >
+      </datatable>
+    </div>
+  </div>
+  </template>
+  
+  <script>
+  import datatable from "@/components/datatable.vue"
+  
+  export default {
+    name: "indexApp",
+    components: { datatable },
+    data() {
+      return {
+        val: null,
+        headers: [
+          "Utilisateur",
+          "Lieu",
+          "Etat"
+        ],
+        items: [
+          { name: 'John Doe', location: "12 rue mes couilles", state: 'john@example.com' },
+          { name: 'Jane Smith', location: "12 rue mes couilles", state: 'jane@example.com' },
+          { name: 'Bob Johnson', location: "12 rue mes couilles", state: 'bob@example.com' },
+        ]
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  </style>
