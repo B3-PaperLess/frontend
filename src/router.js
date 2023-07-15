ad311a833f6e5627610dd8cd924b9672990387a2
tsauvage <ssauvage.timothee@gmail.com>
2023-07-15 18:57:45 +0200

    panel admin

--- src/router.js
+++ src/router.js
@@ -2,6 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router';
 import indexApp from '@/pages/index.vue'
 import indexConnexion from '@/pages/connexion/index.vue'
 import indexHome from '@/pages/home/index.vue'
+import indexAdmin from '@/pages/admin/index.vue'
+import axios from '@/axiosConfig'
+
+const excluded_path = ['/admin', '/home']
 
 const router = createRouter({
     history: createWebHistory(),
@@ -9,7 +13,18 @@ const router = createRouter({
         {path:'/', component: indexApp, name:'index'},
         {path:'/connexion', component: indexConnexion, name:'connexion'},
         {path:'/home', component: indexHome, name:'home'},
+        {path:'/admin', component: indexAdmin, name:'admin'},
     ],
 });
 
+router.beforeEach((to, from, next) => {
+    axios.get('paperless/me').then(({data}) => {}).then(() => {
+        next()
+    }).catch((e) => {
+        if (excluded_path.includes(to.path)) {
+            next('/')
+        }
+        next()
+    })
+})
 export default router;
